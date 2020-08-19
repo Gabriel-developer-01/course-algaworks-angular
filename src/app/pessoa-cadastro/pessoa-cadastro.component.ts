d92116634aad9c66c37a6ccecfecd14a23cbc81f
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-19 10:10:37 -0300

    Desafio_controles, validações e mensagem de erro

--- src/app/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class Formulario{
+nome: string;
+logradouro: string;
+numero: number;
+complemento: string;
+bairro: string;
+cep: number;
+cidade: string;
+estado: string;
+}
 
 @Component({
   selector: 'app-pessoa-cadastro',
   templateUrl: './pessoa-cadastro.component.html',
   styleUrls: ['./pessoa-cadastro.component.css']
 })
-export class PessoaCadastroComponent implements OnInit {
+export class PessoaCadastroComponent  {
 
-  constructor() { }
+  formulario = new Formulario();
 
-  ngOnInit() {
+  salvar(form: NgForm){
+    this.formulario.nome = form.value.nm;
+    this.formulario.logradouro = form.value.lgr;
+    this.formulario.numero = form.value.num;
+    this.formulario.complemento = form.value.comple;
+    this.formulario.bairro = form.value.bairro;
+    this.formulario.cep = form.value.cep;
+    this.formulario.cidade = form.value.cidade;
+    this.formulario.estado = form.value.estado;
+
+    console.log(this.formulario);
   }
-
 }
