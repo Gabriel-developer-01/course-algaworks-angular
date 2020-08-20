6a2be440b263e7ad2622c7617c5e6eabac1dfd0f
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-19 23:55:27 -0300

    Criando um Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoasGridComponent } from './pessoas/pessoas-grid/pessoas-grid.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { MessageComponent } from './message/message.component';
+
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
 import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
-
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 import { CalendarModule } from 'primeng/components/calendar/calendar';
 import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
 import { DropdownModule } from 'primeng/components/dropdown/dropdown';
-import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
-import { FormsModule } from '@angular/forms';
-import { MessageComponent } from './message/message.component';
-import { LancamentosGridComponent } from './lancamentos/lancamentos-grid/lancamentos-grid.component';
-import { PessoasGridComponent } from './pessoas/pessoas-grid/pessoas-grid.component';
-
-
-
-
-
+import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 
 
@@ -35,18 +27,19 @@ import { PessoasGridComponent } from './pessoas/pessoas-grid/pessoas-grid.compon
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
-    LancamentoCadastroComponent,
     PessoaCadastroComponent,
     MessageComponent,
-    LancamentosGridComponent,
     PessoasGridComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+
+    LancamentosModule,
+
     InputTextModule,
     ButtonModule,
     DataTableModule,
@@ -55,9 +48,7 @@ import { PessoasGridComponent } from './pessoas/pessoas-grid/pessoas-grid.compon
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
-    CurrencyMaskModule,
-    InputMaskModule,
-    FormsModule
+    InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
