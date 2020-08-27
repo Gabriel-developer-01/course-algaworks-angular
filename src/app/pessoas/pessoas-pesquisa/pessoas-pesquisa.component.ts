8822e43ac83e4858d5aef8fc59d1ec1e7de02852
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-27 16:49:42 -0300

    Desafio_implementando a exclusão de pessoas

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PessoaService, PessoaFiltro } from '../pessoa.service';
 import { LazyLoadEvent } from 'primeng/components/common/lazyloadevent';
+import { ToastyService } from 'ng2-toasty';
+import { ConfirmationService } from 'primeng/components/common/api';
+import { ErrorHandlerService } from 'app/core/error-handler.service';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -12,23 +15,49 @@ export class PessoasPesquisaComponent implements OnInit {
   totalRegistro = 0;
   filtro = new PessoaFiltro();
   pessoas = [];
+  @ViewChild('tabela') grid;
 
-  constructor(private pessoaService: PessoaService) { }
+  constructor(
+    private pessoaService: PessoaService,
+    private toasty: ToastyService,
+    private confirmation: ConfirmationService,
+    private erroHandler: ErrorHandlerService
+    ) { }
   ngOnInit(): void {
   }
 
   pesquisa(pagina = 0) {
-    this.filtro.pagina =pagina;
+    this.filtro.pagina = pagina;
 
     this.pessoaService.pesquisar(this.filtro)
       .then(resultado => {
         this.pessoas = resultado.pessoas,
           this.totalRegistro = resultado.total
       })
+      .catch(erro => this.erroHandler.handle(erro));
   }
 
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisa(pagina)
+  }
+
+  confirmarExclusao(pessoa: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(pessoa);
+      }
+    });
+  }
+
+  excluir(pessoa: any){
+    this.pessoaService.excluir(pessoa.codigo)
+    .then(() => {
+      this.grid.first === 0 ? this.pesquisa() : this.grid.first = 0;
+
+      this.toasty.success('Pessoa excluída com sucesso!')
+    })
+    .catch(erro => this.erroHandler.handle(erro));
   }
 }
