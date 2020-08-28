87d4d369edf56fef21c2c23ed2bb8fc8bffb8ae5
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-28 10:48:58 -0300

    Desafio_mensagem de erro de usuário na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ToastyService } from 'ng2-toasty';
+import { Response } from '@angular/http';
 
 @Injectable()
 export class ErrorHandlerService {
@@ -8,10 +9,25 @@ export class ErrorHandlerService {
 
   handle(errorResponse: any) {
     let msg: string;
+    console.log('status', errorResponse.status);
+    console.log('body', errorResponse._body);
 
     if(typeof errorResponse === 'string'){
       msg = errorResponse;
-    }else {
+    } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499){
+
+        let errors;
+        msg = 'Ocorreu um erro ao processar a sua solicitação.'
+
+      try {
+        errors = errorResponse.json();
+
+        msg = errors[0].mensagemUsuario;
+
+      } catch (error) {}
+      console.log('Ocorreu um erro: ', errorResponse)
+    }
+    else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.log('Ocorreu um erro',errorResponse);
     }
