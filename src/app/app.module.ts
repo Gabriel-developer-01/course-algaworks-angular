36040195c089c32e10a57773c8fac9a1a740b1b1
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-30 11:24:50 -0300

    Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/navbar/core.module';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 
-
+  const routes: Routes = [
+    { path: 'lancamentos', component: LancamentosPesquisaComponent},
+    { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+    { path: 'pessoas', component: PessoasPesquisaComponent},
+    { path: 'pessoas/novo', component: PessoaCadastroComponent}
+  ];
 
 @NgModule({
   declarations: [
@@ -20,6 +28,7 @@ import { CoreModule } from './core/navbar/core.module';
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
+    RouterModule.forRoot(routes),
 
     LancamentosModule,
     CoreModule,
