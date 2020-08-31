1c8974025e9cd09ec7943dbfc5cb531ced0b7f2d
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-31 15:53:08 -0300

    Refatorando as rotas para usar Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { CoreModule } from './core/core.module';
-import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
-
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
-
-  const routes: Routes = [
-    { path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
-    { path: 'lancamentos', component: LancamentosPesquisaComponent},
-    { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-    { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-    { path: 'pessoas', component: PessoasPesquisaComponent},
-    { path: 'pessoas/novo', component: PessoaCadastroComponent},
-    { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-    { path: '**', redirectTo: 'pagina-nao-encontrada'}
-  ];
 
 @NgModule({
   declarations: [
@@ -34,7 +17,7 @@ import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.compo
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
 
     LancamentosModule,
     CoreModule,
